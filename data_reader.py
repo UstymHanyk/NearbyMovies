d3c71606392d0f09bade0cb32072dfa07add483e
Ustym Hanyk <ustym.hanyk@ucu.edu.ua>
2021-02-15 19:54:03 +0200

    Map generating is working successfully

--- data_reader.py
+++ data_reader.py
@@ -17,7 +17,6 @@ def raw_line_formatter(raw_line:str):
     year = re.findall(pattern, line_list[0])[0]
     year_pos = line_list[0].find(year) -2
     name = line_list[0][:year_pos-1]
-    # year = int(line_list[0][year_pos+1:year_pos+5])
 
     if "(" in line_list[-1]:
         location = line_list[-2]
@@ -25,14 +24,13 @@ def raw_line_formatter(raw_line:str):
         location = line_list[-1].rstrip()
     return (name, int(year), location)
 
-# print(raw_line_formatter('"#15SecondScare" (2015) {Its Me Jessica (#1.5)}\tCoventry, West Midlands, England, UK'))
 def read_data(database:str):
     """
     Reads location database and returns a list of tuples (name, year, location)
     """
     data_obj = open(database, "r", encoding="utf-8", errors="ignore")
     data_list = [raw_line_formatter(line) for line in list(data_obj)[14:-1]]
-    print("Data read")
+    print("Movie data read.")
     return data_list
 
 def select_year(movie_list:list, year:int):
@@ -43,7 +41,5 @@ def select_year(movie_list:list, year:int):
     for movie in movie_list:
         if year in movie:
             n_year_movies.append((movie[0],movie[-1]))
+    print(f"Selected {len(n_year_movies)} movies/shows filmed in {year}.")
     return n_year_movies
-
-# data = read_data("smaller_locations.list")
-# print(len(select_year(data,2010)))
--- locations_finder.py
+++ locations_finder.py
@@ -3,19 +3,12 @@ Finding 10 nearest locations
 """
 from math import radians, cos, sin, asin, sqrt
 from geopy.geocoders import Nominatim
-from geopy.extra.rate_limiter import RateLimiter
-from data_reader import read_data, select_year
 from geopy.exc import GeocoderUnavailable
-
-# data_rawish = read_data("smaller_locations.list")
-# data = select_year(data_rawish, 2014)
-# print(data)
 
 def coord_finder(movie_list):
     """
     Finds coords using geopy
     """
-    print(len(movie_list), "Movies in the year")
     geolocator = Nominatim(user_agent="nearby seymovies")
     movie_loc_list = []
     loc_dict = {}
@@ -27,19 +20,15 @@ def coord_finder(movie_list):
                 if loc_dict[movie[1]] == None:  # generalize the location if geopy can't find it
                     temp_loc_list = movie[1].split(",")
                     shorter_loc = ", ".join(temp_loc_list[1:])
-                    # print(movie, shorter_loc)
                     loc_dict[movie[1]] = geolocator.geocode(shorter_loc)
             except GeocoderUnavailable:
                 loc_dict[movie[1]] = 'unavailable'
         loc = loc_dict.get(movie[1], False)
 
         if loc and loc != 'unavailable':  # if geopy found the location
-            # print(loc.point[:-1])
             loc = loc.point[:-1]
         movie_loc_list.append((movie[0], loc))
     movie_loc_list_clean = [i for i in set(movie_loc_list) if i[1] != None and i[1] != 'unavailable']
-    print(len(movie_loc_list_clean), "Locations found")
-    print(movie_loc_list_clean)
     return movie_loc_list_clean
 
 def haversine(lat1, lon1, lat2, lon2):
@@ -67,11 +56,6 @@ def find_nearest_movies(movie_loc_list, user_loc: tuple):
     for movie in movie_loc_list:
         distance = haversine(float(movie[1][0]), float(movie[1][1]), user_loc[0], user_loc[1])
         distances.append(distance)
-        # print(distance)
     movie_distances = list(zip(movie_loc_list,distances))
-    # print(movie_distances)
+    print("10 nearest movies found.")
     return sorted(movie_distances,key = lambda i: i[1])[:10]
-movie_locations = [('"#ATown', (30.2711286, -97.7436995)), ('"#ATown', (30.2711286, -97.7436995)), ('"#ATown', (29.8826436, -97.9405828)), ('"#ATown', (30.268603249999998, -97.76277328657375)), ('"#ATown', (30.27213615, -97.7688868111702)), ('"#ATown', (30.3207674, -97.7733474)), ('"#ATown', (30.2711286, -97.7436995)), ('"#ATown', (30.240283, -97.7887279)), ('"#ATown', (30.2020961, -97.6700119)), ('"#Elmira', (42.0897965, -76.8077338)), ('"#Nightstrife', (41.8755616, -87.6244212)), ('"(Des)Encontros', (-23.9661602, -46.3287265)), ('"(Mis)adventure', (34.1729044, -118.3740371)), ('"(Mis)adventure', (34.0536909, -118.242766)), ('"(gli) Imperatori', (41.8933203, 12.4829321)), ('"+27: Social Innovators of South Africa', (-33.928992, 18.417396)), ('"+27: Social Innovators of South Africa', (-26.205, 28.049722)), ('"/Drive on NBCSN', (46.5286176, 10.4532056)), ('"/Drive on NBCSN', (51.3201891, -0.5564726)), ('"/Drive on NBCSN', (24.4538352, 54.3774014)), ('"/Drive on NBCSN', (37.320774, -113.00484758652519)), ('"/Drive on NBCSN', (36.1672559, -115.1485163)), ('"/Drive on NBCSN', (36.2083012, -115.9839128)), ('"/Drive on NBCSN', (36.4951365, -116.421881)), ('"/Drive on NBCSN', (36.4914385, -117.10229360771876)), ('"/Drive on NBCSN', (37.84054795, -119.51658779802511)), ('"10 Films, 1 Box', (34.0536909, -118.242766)), ('"100 Mile Meals', (37.050096, -121.9905908)), ('"15 minutos de fama', (40.7127281, -74.0060152)), ('"2 Awkward Dudes', (40.7127281, -74.0060152))]
-print(find_nearest_movies(movie_locations,(36.2672559, -115.1485163)))
-
-
